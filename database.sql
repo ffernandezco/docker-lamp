7ea99c201fd781025b84a04d68fd11552d13d3a9
Xabier Unzilla <xabierunzilla@gmail.com>
2024-09-28 11:42:41 +0200

    base de datos= tabla alimentos creada + alimentos agregados y usuarios

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: db:3306
--- Tiempo de generación: 26-09-2024 a las 09:38:10
+-- Tiempo de generación: 28-09-2024 a las 09:31:53
 -- Versión del servidor: 10.8.2-MariaDB-1:10.8.2+maria~focal
 -- Versión de PHP: 8.2.8
 
@@ -20,6 +20,32 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `database`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alimentos`
+--
+
+CREATE TABLE `alimentos` (
+  `id` int(11) NOT NULL,
+  `nombre` text NOT NULL,
+  `fcompra` date NOT NULL,
+  `fcaducidad` date NOT NULL,
+  `calorias` int(11) NOT NULL,
+  `precio` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `alimentos`
+--
+
+INSERT INTO `alimentos` (`id`, `nombre`, `fcompra`, `fcaducidad`, `calorias`, `precio`) VALUES
+(1, 'Manzana', '2024-09-28', '2024-10-05', 300, 1.5),
+(2, 'Macarrones', '2024-09-29', '2025-01-11', 500, 0.9),
+(3, 'Tomate', '2024-09-28', '2024-10-05', 100, 0.97),
+(4, 'Chorizo', '2024-09-29', '2024-11-13', 800, 3),
+(5, 'Queso', '2024-08-14', '2024-09-30', 400, 4.9);
 
 -- --------------------------------------------------------
 
@@ -43,11 +69,20 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `dni`, `nombre`, `apellidos`, `tel`, `fechanacimiento`, `email`, `password`) VALUES
-(1, '71314492W', 'Francisco', 'Fernandez Condado', 622622622, '2003-04-17', 'ffernandez032@ikasle.ehu.eus', 'fran1234');
+(1, '71314492W', 'Francisco', 'Fernandez Condado', 622622622, '2003-04-17', 'ffernandez032@ikasle.ehu.eus', 'fran1234'),
+(2, '79135981L', 'Diego', 'Gonzalez Tamayo', 619725058, '2024-09-24', 'dieguito.ander@yahoo.es', 'KaixoPatxi13'),
+(3, '79008700C', 'Xabier', 'Unzilla Higuero', 684007082, '2024-08-01', 'xunzilla@ikasle.ehu.eus', 'Xabier2024');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `alimentos`
+--
+ALTER TABLE `alimentos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -61,10 +96,16 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `alimentos`
+--
+ALTER TABLE `alimentos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
