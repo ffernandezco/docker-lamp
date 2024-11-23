ce3d264d8fcbab9c90a0a01533a2720db3217550
diego <diegogonza0305@gmail.com>
2024-11-23 18:23:17 +0100

    en login.php se ha agregado codigo para que cuando alguien se logea se genere una cookie con la flag httpOnly y atributo samesite

--- app/login.php
+++ app/login.php
@@ -1,4 +1,15 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['enviar'])) {
+    $email = htmlentities($_POST['email']);
 
+    // Configuración de la cookie con 1 hora de duración
+    setcookie('email', $email, time() + 3600, "/; SameSite=Strict", "", true, true);
+
+    // Redirigir a index.php
+    header("Location: index.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -12,16 +23,16 @@
         <div class="card shadow p-5">
             <h1 class="text-center mb-4">¡Hola!</h1>
             <p class="text-center">Inicia sesión con tu correo electrónico y contraseña</p>
-            <form id="login_form" action="login_handler.php" method="POST">
+            <form method="POST" id="login_form" action="">
                 <div class="form-group mb-3">
                     <label for="email">Correo electrónico</label>
-                    <input type="email" class="form-control" id="email" name="email" placeholder="usuario@dominio.com" required>
+                    <input type="text" class="form-control" id="email" name="email" placeholder="usuario@dominio.com" required>
                 </div>
                 <div class="form-group mb-3">
                     <label for="password">Contraseña</label>
                     <input type="password" class="form-control" id="password" name="password" placeholder="**********" required>
                 </div>
-                <button type="submit" class="btn btn-primary w-100 mt-3" id="login_submit">Iniciar sesión</button>
+                <button type="submit" name="enviar" class="btn btn-primary w-100 mt-3" value="enviar" id="login_submit">Iniciar sesión</button>
             </form>
             <div class="mt-5 text-center text-dark">
                 <p>¿No tienes cuenta? <a href="/register">Regístrate ahora</a></p>
